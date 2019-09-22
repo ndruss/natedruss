42136101009c32d513fcf450fb9ceaefc1645780
nate <nate@nsideas.com>
2019-09-22 12:25:54 -0400

    Added header css module

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
+import styles from "./header.module.css"
 
 export default () => (
-  <header>
+  <header className={styles.header}>
     <h1><Link to="/">Nate Druss</Link></h1>
     <nav>
       <ul>
