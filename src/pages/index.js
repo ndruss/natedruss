3019cbec789242817a333ba3d1d3701cd6264ed8
nate <nate@nsideas.com>
2019-09-22 14:02:32 -0400

    Sticky header

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,22 @@ import React from "react"
 import Layout from "../components/layout"
 import Intro from "../components/site-intro"
 
+const Tile = () => (
+  <div className="tile"></div>
+)
+
 export default () => (
   <>
     <Intro />
     <Layout>
       <div id="work" className="contain">
         <p>This is the main content of my home page. Here, you will find a list of recent projects and maybe some links.</p>
+
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
       </div>
     </Layout>
   </>
