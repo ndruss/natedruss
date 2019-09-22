deb69705f2066f4df520f671a76cafaa86b0c575
nate <nate@nsideas.com>
2019-09-21 22:12:54 -0400

    Added info page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div class="page-home">
+    <section className="site-intro">
+      <header>
+        <h1>Hi, I'm Nate</h1>
+        <h2>a designer / developer in Cleveland, Ohio.</h2>
+      </header>
+    </section>
+    <main className="main-content">
+      <p>This is the main content of my home page. Here, you will find a list of recent projects and maybe some links.</p>
+    </main>
+  </div>
+)
---
+++ src/pages/info.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default () => (
+  <div class="page-about">
+    <main className="main-content">
+      <h1>About Me</h1>
+      <p>My name is Nate Druss and I am a multi-disciplinary designer, currently focused on digital media. My passion lies at the intersection of creativity and technology. I'm a big fan of clean code and user-friendly design. If You'd like to get in touch, contact me at nhdruss@gmail.com</p>
+    </main>
+  </div>
+)
