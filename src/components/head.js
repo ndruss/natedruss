07c2ea36bb414d060ff7bb9fc8ef95c601b5f246
nate <nate@nsideas.com>
2019-09-29 00:48:36 -0400

    Added Source Code Pro font

--- src/components/head.js
+++ src/components/head.js
@@ -5,5 +5,6 @@ export default props => (
   <Helmet>
     <title>{props.title} | Nate Druss</title>
     <link rel="canonical" href="http://mysite.com/example" />
+    <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro:300,300i,500,500i,700,700i&display=swap" rel="stylesheet" />
   </Helmet>
 )
