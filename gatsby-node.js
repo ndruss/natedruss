e4989ef3d4c18ffeb8c98dd5a67cd7bb9181c031
nate <nate@nsideas.com>
2020-02-09 14:15:29 -0500

    Create pages from data

---
+++ gatsby-node.js
@@ -0,0 +1,10 @@
+exports.createPages = ({ actions: { createPage } }) => {
+  createPage({
+    path: "/page-with-context/",
+    component: require.resolve("./src/templates/with-context.js"),
+    context: {
+      title: "We Don’t Need No Stinkin’ GraphQL!",
+      content: "<p>This is page content.</p><p>No GraphQL required!</p>",
+    },
+  })
+}
