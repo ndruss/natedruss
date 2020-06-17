5122d32e78f80c921511a098cb7e84b988d6f350
nate <nate@nsideas.com>
2020-06-17 19:55:17 -0400

    Delete Tile.js

--- src/components/Tile.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-export const Tile = props => (
-  <div className="tile">
-    <h2>{props.title}</h2>
-    <h3>{props.description}</h3>
-    <div>{props.content}</div>
-  </div>
-)
