5deb469c147bd5d464792efc15c96b20f7ecf23e
nate <nate@nsideas.com>
2020-06-30 20:43:52 -0400

    Added project tag list module

---
+++ src/components/tag-list/tag-list.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./tag-list.module.scss"
+
+export default function TagList(props) {
+  return props.tags ? (
+    <ul className={styles.list}>
+      {props.tags.map(tag =>
+        <li className={styles.item}>{tag}</li>
+      )}
+    </ul>
+  ) : '';
+}
--- src/templates/project.js
+++ src/templates/project.js
@@ -4,19 +4,23 @@ import Layout from "../components/layout"
 import Head from "../components/head"
 import Thumbnail from "../components/thumbnail/thumbnail"
 import styles from "./project.module.scss"
+import TagList from "../components/tag-list/tag-list"
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
-  const { markdownRemark } = data // data.markdownRemark holds your post data
+  // data.markdownRemark holds your post data
+  const { markdownRemark } = data
   const { frontmatter, html } = markdownRemark
   return (
     <Layout>
       <Head title={frontmatter.title} />
-      <article>
-        <header className={`container--wide ${styles.header}`}>
+
+      <article className={styles.article}>
+        <header className={`container ${styles.header}`}>
           <h1>{frontmatter.title}</h1>
           <p className={styles.description}>{frontmatter.description}</p>
+          <TagList tags={frontmatter.tags}/>
         </header>
         <div className="container--wide">
           <Thumbnail frontmatter={frontmatter} />
@@ -26,6 +30,7 @@ export default function Template({
           dangerouslySetInnerHTML={{ __html: html }}
         />
       </article>
+
     </Layout>
   )
 }
