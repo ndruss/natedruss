5512ac3d4ed6f8bb8acf5b291f7407cfd04ad8d0
Nate Druss <nate@nsideas.com>
2020-07-06 19:37:50 -0400

    Convert tags to string

--- src/components/tag-list/index.js
+++ src/components/tag-list/index.js
@@ -3,10 +3,10 @@ import styles from "./tag-list.module.scss"
 
 export default function TagList(props) {
   return props.tags ? (
-    <ul className={styles.list}>
+    <p className={styles.list}>
       {props.tags.map(tag =>
-        <li key={tag} className={styles.item}>{tag}</li>
+        <span key={tag} className={styles.item}>{tag}</span>
       )}
-    </ul>
+    </p>
   ) : '';
 }
