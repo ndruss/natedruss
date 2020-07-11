0a15d9365f0fed99bf9b98ff02872d2f44d45f6f
nate <nate@nsideas.com>
2020-07-10 20:01:25 -0400

    Add content to info page

--- src/pages/info.js
+++ src/pages/info.js
@@ -8,9 +8,11 @@ export default ({ data }) => (
     <Head title="Info" />
     <div className="container">
       <h1 className="page-title">About Me</h1>
-      <p>
-        My name is Nate Druss and I am a multi-disciplinary designer, currently focused on digital media. My passion lies at the intersection of creativity and technology. I'm a big fan of clean code and user-friendly design. If You'd like to get in touch, contact me at nhdruss@gmail.com.
-      </p>
+      <div class="body-text">
+        <p>
+          My name is Nate Druss and I work as a Designer/Developer at <a className="nowrap" href="https://nsideas.com">Nesnadny + Schwartz</a>.
+        </p>
+      </div>
     </div>
   </Layout>
 )
