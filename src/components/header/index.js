d7e4a7bc7a8205416b832a4e944f2147c82a2d22
nate <nhdruss@gmail.com>
2021-04-26 14:34:35 -0400

    Improved home link handling

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,47 +1,28 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { homeLinkClick, hashLinkClick } from '../../utils'
 import styles from './header.module.scss'
 import Logo from '../../images/svg/nate-druss-logo.svg'
-
-const ListLink = props => {
-  const handleClick = e => {
-    e.preventDefault()
-
-    if (e.target.hash && document.querySelector(e.target.hash)) {
-      if (!window.location.hash) {
-        window.history.pushState({ content: 'work' }, '', e.target.hash)
-      }
-
-      document
-        .querySelector(e.target.hash)
-        .scrollIntoView({ behavior: 'smooth' })
-    } else {
-      window.location = e.target.href
-    }
-  }
-
-  return (
-    <li className={styles.listItem}>
-      <Link onClick={handleClick} to={props.to}>
-        {props.children}
-      </Link>
-    </li>
-  )
-}
 
 export default () => (
   <header className={styles.header}>
     <div className="container container--full">
       <div className={styles.logo}>
-        <Link to="/">
+        <Link onClick={homeLinkClick} to="/">
           <Logo />
           <span className="screen-reader-text">Nate Druss</span>
         </Link>
       </div>
       <nav>
         <ul className={styles.list}>
-          <ListLink to="/#work">Work</ListLink>
-          <ListLink to="/info">Info</ListLink>
+          <li className={styles.listItem}>
+            <Link onClick={hashLinkClick} to="/#work">
+              Work
+            </Link>
+          </li>
+          <li className={styles.listItem}>
+            <Link to="/info">Info</Link>
+          </li>
         </ul>
       </nav>
     </div>
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,32 @@
-export function camelize(str) {
-  return str
-    .replace(/(?:^\w|[A-Z]|\b\w)/g, function(word, index) {
-      return index === 0 ? word.toLowerCase() : word.toUpperCase()
-    })
-    .replace(/\s+/g, '')
+import { navigate, withPrefix } from 'gatsby'
+
+export const homeLinkClick = e => {
+  e.preventDefault()
+  if (window.location.pathname !== withPrefix('/')) {
+    navigate('/')
+    return
+  }
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: 'smooth',
+  })
+  if (!!window.location.hash) {
+    window.history.pushState({ content: 'home' }, '', '/')
+  }
+}
+
+export const hashLinkClick = e => {
+  e.preventDefault()
+  if (e.target.pathname !== window.location.pathname) {
+    navigate(e.target.pathname + e.target.hash)
+    return
+  }
+  const section = document.querySelector(e.target.hash)
+  if (section) {
+    section.scrollIntoView({ behavior: 'smooth' })
+  } else {
+    window.location = e.target.href
+  }
+  window.history.pushState({ content: e.target.hash }, '', e.target.hash)
 }
